4995f2371fdf0bd110edd47adfdb6177e8b01714
gwendal le bris <gwen.coding@gmail.com>
2023-09-14 16:39:47 +0200

    Mustache Iteration

--- javascript/application.js
+++ javascript/application.js
@@ -2,17 +2,27 @@ console.log("hello from application.js")
 
 import Mustache from "mustachejs";
 
+const template = document.querySelector("#movieCardTemplate").innerHTML
+const results = document.querySelector("#results")
+
 fetch("http://www.omdbapi.com/?s=harry potter&apikey=adf1f2d7")
   .then(response => response.json())
   .then((data) => {
-    const results = document.querySelector('#results');
-    const template = document.querySelector("#movieCardTemplate").innerHTML;
-    data.Search.forEach((movie) => {
-      const output = Mustache.render(template, movie);
-      results.insertAdjacentHTML("beforeend", output)
-
-    })
+    const movieData = {"movies": data.Search}
+    const output = Mustache.render(template, movieData);
+    results.innerHTML = output;
   })
+
+// fetch("http://www.omdbapi.com/?s=harry potter&apikey=adf1f2d7")
+//   .then(response => response.json())
+//   .then((data) => {
+//     const results = document.querySelector('#results');
+//     const template = document.querySelector("#movieCardTemplate").innerHTML;
+//     data.Search.forEach((movie) => {
+//       const output = Mustache.render(template, movie);
+//       results.insertAdjacentHTML("beforeend", output)
+//     })
+//   })
 
 // fetch("http://www.omdbapi.com/?s=harry potter&apikey=adf1f2d7")
 //   .then(response => response.json())
