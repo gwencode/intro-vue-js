2a6c4fd3867087c9d97f0db55ceafe5f4cbd216d
gwendal le bris <gwen.coding@gmail.com>
2023-09-14 16:12:48 +0200

    Discover <template>

--- javascript/application.js
+++ javascript/application.js
@@ -1,1 +1,19 @@
 console.log("hello from application.js")
+
+import Mustache from "mustachejs";
+
+fetch("http://www.omdbapi.com/?s=harry potter&apikey=adf1f2d7")
+  .then(response => response.json())
+  .then((data) => {
+    const results = document.querySelector('#results');
+    const template = document.querySelector("#movieCardTemplate");
+    data.Search.forEach((movie) => {
+      const clone = template.content.cloneNode(true);
+      clone.querySelector("img").src = movie.Poster;
+      clone.querySelector("h6").textContent = movie.Title;
+      clone.querySelector("p").textContent = movie.Year;
+      clone.querySelector("a").href = `https://www.imdb.com/title/${movie.imdbID}`;
+      results.appendChild(clone);
+    })
+    console.log(data.Search);
+  })
