93aac8c3535b82ec4e618ef90fb23548700ecf43
gwendal le bris <gwen.coding@gmail.com>
2023-09-14 16:28:32 +0200

    Discover Mustache

--- javascript/application.js
+++ javascript/application.js
@@ -6,14 +6,25 @@ fetch("http://www.omdbapi.com/?s=harry potter&apikey=adf1f2d7")
   .then(response => response.json())
   .then((data) => {
     const results = document.querySelector('#results');
-    const template = document.querySelector("#movieCardTemplate");
+    const template = document.querySelector("#movieCardTemplate").innerHTML;
     data.Search.forEach((movie) => {
-      const clone = template.content.cloneNode(true);
-      clone.querySelector("img").src = movie.Poster;
-      clone.querySelector("h6").textContent = movie.Title;
-      clone.querySelector("p").textContent = movie.Year;
-      clone.querySelector("a").href = `https://www.imdb.com/title/${movie.imdbID}`;
-      results.appendChild(clone);
+      const output = Mustache.render(template, movie);
+      results.insertAdjacentHTML("beforeend", output)
+
     })
-    console.log(data.Search);
   })
+
+// fetch("http://www.omdbapi.com/?s=harry potter&apikey=adf1f2d7")
+//   .then(response => response.json())
+//   .then((data) => {
+//     const results = document.querySelector('#results');
+//     const template = document.querySelector("#movieCardTemplate");
+//     data.Search.forEach((movie) => {
+//       const clone = template.content.cloneNode(true);
+//       clone.querySelector("img").src = movie.Poster;
+//       clone.querySelector("h6").textContent = movie.Title;
+//       clone.querySelector("p").textContent = movie.Year;
+//       clone.querySelector("a").href = `https://www.imdb.com/title/${movie.imdbID}`;
+//       results.appendChild(clone);
+//     })
+//   })
